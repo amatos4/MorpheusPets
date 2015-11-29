e6d0f58af3e06f787d5ac0a0ff997b7971870c93
amatos4 <amatos@stevens.edu>
2015-11-29 14:50:29 -0500

    My Profile and stock images
    standard my profile page. css file needs to be added onto to make this
    formatted again

--- my_profile.php
+++ my_profile.php
@@ -2,9 +2,11 @@
 <!--[if IE 7]><html class="ie ie7" lang="en"><![endif]-->
 <!--[if IE 8]><html class="ie ie8" lang="en"><![endif]-->
 <!--[if IE 9]><html class="ie ie9" lang="en"><![endif]-->
-<!--[if gt IE 9]><!--><html class="no-js" lang="en"><!--<![endif]-->
+<!--[if gt IE 9]><!-->
+<html class="no-js" lang="en">
+<!--<![endif]-->
 
-  <head>
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="author" content="Title">
@@ -20,23 +22,69 @@
     <script src="static/js/lib/modernizr.js"></script>
 
     <title>H&amp;L</title>
-  </head>
+</head>
 
-  <body>
+<body>
+
+    <nav>
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="#">My Profile</a></li>
+            <li class="logout"><a href="#">Logout</a></li>
+        </ul>
+    </nav>
 
     <div id="container">
 
+       <section class="profile-badge">
+            <h1>John Smith</h1>
+            <p>About Me...</p>
+        </section>
 
+        <section class="profile-badge">
+            <ul>
+                <li class="pet-badge">
+                    <img src="images/bird.jpg" />
+                    <h1>Parakeet</h1>
+                    <p>This is a fun one.</p>
+                </li>
+                <li class="pet-badge">
+                    <img src="images/city.jpg" />
+                    <h1>Big City</h1>
+                    <p>We all got dreams.</p>
+                </li>
+                <li class="pet-badge">
+                    <img src="images/frog.jpg" />
+                    <h1>Frog Junior</h1>
+                    <p>He packs a punch.</p>
+                </li>
+                <li class="pet-badge">
+                    <img src="images/frog.jpg" />
+                    <h1>Frog Senior</h1>
+                    <p>Mean, Lean, and Green.</p>
+                </li>
+                <li class="pet-badge">
+                    <img src="images/bird.jpg" />
+                    <h1>Bird</h1>
+                    <p>He's a big bird and he does not live on Sesame Street.</p>
+                </li>
+                <li class="pet-badge">
+                    <img src="images/city.jpg" />
+                    <h1>Little City</h1>
+                    <p>Little City Still Pretty Big Dreams.</p>
+                </li>
+            </ul>
+        </section>
 
     </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script>
-      window.jQuery || document.write('<script src="static/js/lib/jquery.js"><\/script>')
+        window.jQuery || document.write('<script src="static/js/lib/jquery.js"><\/script>')
     </script>
     <script src="static/js/plugins.js"></script>
     <script src="static/js/base.js"></script>
 
-  </body>
+</body>
 
 </html>

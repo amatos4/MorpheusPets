f44af4de694f066dfbeef1ce034733a0a44aafa5
amatos4 <amatos@stevens.edu>
2015-11-29 21:15:06 -0500

    Updated My Profile and base css file

--- my_profile.php
+++ my_profile.php
@@ -21,7 +21,7 @@
 
     <script src="static/js/lib/modernizr.js"></script>
 
-    <title>H&amp;L</title>
+    <title>My MorpheusPets Profile</title>
 </head>
 
 <body>
@@ -30,6 +30,8 @@
         <ul>
             <li><a href="index.html">Home</a></li>
             <li><a href="#">My Profile</a></li>
+            <input id="search-bar" name="search" type="text" placeholder="Search...">
+            <input id="search-button" name="search_submit" type="submit" value="Go">
             <li class="logout"><a href="#">Logout</a></li>
         </ul>
     </nav>
@@ -37,41 +39,61 @@
     <div id="container">
 
        <section class="profile-badge">
-            <h1>John Smith</h1>
-            <p>About Me...</p>
+           <img src="images/avatar_blank.png"/>
+           <article class="description">
+                <h1>@John_Smith</h1>
+                <p>I am considered one of the very best players in the entire game. I am out to create the best team of MorpheusPets! Battle against me and see if you have what it takes.</p>
+           </article>
         </section>
 
-        <section class="profile-badge">
+        <section class="pet-container">
+            <h1>My Pet Collection</h1>
             <ul>
                 <li class="pet-badge">
-                    <img src="images/bird.jpg" />
-                    <h1>Parakeet</h1>
-                    <p>This is a fun one.</p>
+                    <a href="#"><img src="images/bird.jpg" /></a>
+                    <div class="pet-stats" />
+                        <h1>Parakeet</h1>
+                        <p><b>Species: </b>Charmander</p>
+                        <p><b>Type: </b> Fire</p>
+                        <p><b>Active: </b>Yes</p>
+                    </div>
                 </li>
                 <li class="pet-badge">
                     <img src="images/city.jpg" />
-                    <h1>Big City</h1>
-                    <p>We all got dreams.</p>
+                    <div class="pet-stats" />
+                        <h1>Big City</h1>
+                        <p>Species: Squirtle</p>
+                        <p>Type: Water</p>
+                    </div>
                 </li>
                 <li class="pet-badge">
                     <img src="images/frog.jpg" />
-                    <h1>Frog Junior</h1>
-                    <p>He packs a punch.</p>
+                    <div class="pet-stats" />
+                        <h1>Frog Junior</h1>
+                        <p>Species: Frog</p>
+                        <p>Type: Amphibian</p>
+                    </div>
                 </li>
                 <li class="pet-badge">
                     <img src="images/frog.jpg" />
-                    <h1>Frog Senior</h1>
-                    <p>Mean, Lean, and Green.</p>
+                    <div class="pet-stats" />
+                        <h1>Frog Senior</h1>
+                        <p>Mean, Lean, and Green.</p>
+                    </div>
                 </li>
                 <li class="pet-badge">
                     <img src="images/bird.jpg" />
-                    <h1>Bird</h1>
-                    <p>He's a big bird and he does not live on Sesame Street.</p>
+                    <div class="pet-stats" />
+                        <h1>Bird</h1>
+                        <p>He's a big bird and he does not live on Sesame Street.</p>
+                    </div>
                 </li>
                 <li class="pet-badge">
                     <img src="images/city.jpg" />
-                    <h1>Little City</h1>
-                    <p>Little City Still Pretty Big Dreams.</p>
+                    <div class="pet-stats" />
+                        <h1>Little City</h1>
+                        <p>Little City Still Pretty Big Dreams.</p>
+                    </div>
                 </li>
             </ul>
         </section>

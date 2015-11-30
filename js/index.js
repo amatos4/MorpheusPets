1abb845642f74250c503ca51e2f3d5ef2ef23904
mpeleshenko <mpeleshenko@gmail.com>
2015-11-29 20:51:05 -0500

    Add JavaScript

---
+++ js/index.js
@@ -0,0 +1,6 @@
+//TODO: Update to activate navigation tab if needed
+//(function($)
+//{
+//    // Activate Home navigation tag
+//    $(".nav #home").addClass("active");
+//})($);
---
+++ js/login.js
@@ -0,0 +1,6 @@
+//TODO: Update to activate navigation tab if needed
+//(function($)
+//{
+//    // Activate Log in navigation tag
+//    $(".nav #login").addClass("active");
+//})($);

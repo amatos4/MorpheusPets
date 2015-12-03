51f164ca69c73c94e73f2275061e0462afa9c807
amatos4 <amatos@stevens.edu>
2015-12-03 12:33:19 -0500

    Wrapped javascript for user_profile
    Anonymous function wrap for user_profile javascript

--- js/user_profile.js
+++ js/user_profile.js
@@ -1,15 +1,18 @@
 /**
  * Created by Anthony on 12/1/2015.
  */
-$(document).ready(function() {
-    $(".small-btn").click(function(e){
-        e.preventDefault();
-        var content = document.getElementById("user-description").innerText;
-        $('#user-description').replaceWith('<form class="description-edit" enctype="multipart/form-data" action="#" method="POST">' +
-            '<input id="description-text" type="text" value=""/>' +
-            '<input type="submit" name="Submit"/>' +
-            '</form>');
-        document.getElementById("description-text").value = content;
-    });
+(function ( $ )
+{
+    $(document).ready(function() {
+        $(".small-btn").click(function(e){
+            e.preventDefault();
+            var content = document.getElementById("user-description").innerText;
+            $('#user-description').replaceWith('<form class="description-edit" enctype="multipart/form-data" action="#" method="POST">' +
+                '<input id="description-text" type="text" value=""/>' +
+                '<input type="submit" name="Submit"/>' +
+                '</form>');
+            document.getElementById("description-text").value = content;
+        });
 
-});
+    });
+})( $ );

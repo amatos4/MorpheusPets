8ddb4946344a8fefab56a5579de92df64160971b
amatos4 <amatos@stevens.edu>
2015-12-02 22:37:06 -0500

    Fixed error with prepared statement and settings
    Settings is defaulted back to no password.
    The prepared statements for active pets is also fixed as well.

--- data/settings.php
+++ data/settings.php
@@ -2,5 +2,5 @@
     $db_connection = array();
     $db_connection['host'] = "localhost";
     $db_connection['username'] = "root"; // Change this to your user
-    $db_connection['password'] = "cs546"; // randomly generated
+    $db_connection['password'] = ""; // randomly generated (previously cs546)
     $db_connection['database'] = "morpheus_pets"; // randomly generated

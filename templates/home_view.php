14728386a3ebaa4937e597f17ee05acccb49921e
Divy Tolia <divystolia@gmail.com>
2015-12-07 18:55:46 -0500

    added logo, text animation for welcome page

--- templates/home_view.php
+++ templates/home_view.php
@@ -2,9 +2,15 @@
 <html>
 <head>
   <title></title>
-  <link rel="stylesheet" type="text/css" href="./.css">
+  <link rel="stylesheet" href="./base.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 </head>
 <body>
-<img src="./logo.png" height="250px" width="512px"/>
+<img id= "logo" src="./logo.png"/>
+  <div class='text'>
+  <div class='content'></div>
+  <div class='dash'></div>
+</div>
+<script src="./thrill.js"></script>
 </body>
 </html>
---
+++ thrill.js
@@ -0,0 +1,26 @@
+(function () {
+    var app;
+    $(document).ready(function () {
+        return app.init();
+    });
+    app = {
+        text: 'Are you ready to battle?',
+        index: 0,
+        chars: 0,
+        speed: 100,
+        container: '.text .content',
+        init: function () {
+            this.chars = this.text.length;
+            return this.write();
+        },
+        write: function () {
+            $(this.container).append(this.text[this.index]);
+            if (this.index < this.chars) {
+                this.index++;
+                return window.setTimeout(function () {
+                    return app.write();
+                }, this.speed);
+            }
+        }
+    };
+}.call(this));

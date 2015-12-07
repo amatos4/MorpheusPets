3840d9b804bf3b821af9d2e3d5f1aaaac6300ab2
mpeleshenko <mpeleshenko@gmail.com>
2015-12-07 13:18:40 -0500

    Clean up header for validation by removing form from ul, change button size to fit better in header

--- templates/header.php
+++ templates/header.php
@@ -1,14 +1,18 @@
 <?php
 
-/** @var User $logged_in_user */
-$logged_in_user = isset( $data[ 'logged_in_user' ] ) ? $data[ 'logged_in_user' ] : null;
+  /** @var User $logged_in_user */
+  $logged_in_user = isset( $data[ 'logged_in_user' ] ) ? $data[ 'logged_in_user' ] : null;
 
 ?>
 <!DOCTYPE html>
-<!--[if IE 7]><html class="ie ie7" lang="en"><![endif]-->
-<!--[if IE 8]><html class="ie ie8" lang="en"><![endif]-->
-<!--[if IE 9]><html class="ie ie9" lang="en"><![endif]-->
-<!--[if gt IE 9]><!--><html class="no-js" lang="en"><!--<![endif]-->
+<!--[if IE 7]>
+<html class="ie ie7" lang="en"><![endif]-->
+<!--[if IE 8]>
+<html class="ie ie8" lang="en"><![endif]-->
+<!--[if IE 9]>
+<html class="ie ie9" lang="en"><![endif]-->
+<!--[if gt IE 9]><!-->
+<html class="no-js" lang="en"><!--<![endif]-->
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
@@ -27,22 +31,35 @@ $logged_in_user = isset( $data[ 'logged_in_user' ] ) ? $data[ 'logged_in_user' ]
   <body>
 
     <nav>
+      <ul class="navbar-left">
+        <li class><a href="index.php">Home</a></li>
+        <?php if ( $logged_in_user !== null ) : ?>
+          <li><a href="my_profile.php?profileId=<?php echo $logged_in_user->getId() ?>">My Profile</a></li>
+          <li><a href="pet_editor.php">Create Pet</a></li>
+        <?php endif; ?>
+      </ul>
+
+      <div class="navbar-right">
+        <form class="navbar-form" id="search_form" enctype="multipart/form-data" action="search.php"
+              method="POST">
+          <div class="form-group">
+            <input class="form-control" id="search-bar" name="search" type="text" placeholder="Search User..." required>
+            <input class="btn" id="search-button" name="search_submit" type="submit" value="Go">
+          </div>
+
+        </form>
+
         <ul>
-            <li><a href="index.php">Home</a></li>
-            <?php if ( !is_null( $logged_in_user )) : ?>
-            <li><a href="my_profile.php?profileId=<?php echo $logged_in_user->getId() ?>">My Profile</a></li>
-            <li class="logout"><a href="logout.php">Logout</a></li>
-            <li class="logout"><a href="pet_editor.php">Create Pet</a></li>
-            <?php endif; ?>
-            <form id="search_form" enctype="multipart/form-data" action="search.php" method="POST">
-                <input id="search-bar" name="search" type="text" placeholder="Search..." required>
-                <input id="search-button" name="search_submit" type="submit" value="Go">
-            </form>
-            <?php if ( is_null( $logged_in_user )) : ?>
-            <li class="login"><a href="login.php">Login</a></li>
-            <li class="register"><a href="register.php">Register</a></li>
-            <?php endif; ?>
+          <?php if ( $logged_in_user === null ) : ?>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="register.php">Register</a></li>
+          <?php else: ?>
+            <li><a href="logout.php">Logout</a></li>
+          <?php endif; ?>
         </ul>
+      </div>
+
+
     </nav>
 
     <div id="container">

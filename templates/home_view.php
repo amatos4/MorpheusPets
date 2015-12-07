a928268e426a029de24ca7cce8189eb290082fdd
Divy Tolia <divystolia@gmail.com>
2015-12-07 17:48:07 -0500

    added logo and home view php

---
+++ templates/home_view.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <link rel="stylesheet" type="text/css" href="./.css">
+</head>
+<body>
+<img src="./logo.png" height="250px" width="512px"/>
+</body>
+</html>

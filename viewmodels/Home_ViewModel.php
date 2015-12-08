b96b196e4103a4f5df88963f3dee8ed8972c56a6
Divy Tolia <divystolia@gmail.com>
2015-12-07 20:42:55 -0500

    BROKEN CODE!!!! FIX IT

--- viewmodels/Home_ViewModel.php
+++ viewmodels/Home_ViewModel.php
@@ -3,6 +3,7 @@
 
   class Home_ViewModel extends ViewModel
   {
+    private user_list = [ ];
     /*
      * Render the home page
      */
@@ -10,9 +11,18 @@
     {
       $view_data[ 'page_title' ] = 'Home';
       $view_data[ 'js' ]         = '<script src="js/index.js"></script>';
+      $view_data[ 'recent_users'] = $user_list;
 
       $this->renderTemplate( 'templates/header.php', $view_data );
       $this->renderTemplate( 'templates/home_view.php', $view_data );
       $this->renderTemplate( 'templates/footer.php', $view_data );
     }
+  public function addRecentUsers( $users )
+    {
+      if ( isset( $users ) )
+      {
+        array_push( $this->user_list,$users );
+      }
+    }
+
   }

79dde6333192fa94b5f4ff1a2f43d6143ea8a45c
mpeleshenko <mpeleshenko@gmail.com>
2015-12-07 22:06:48 -0500

    Revert changes to settings.php

--- data/settings.php
+++ data/settings.php
@@ -2,5 +2,5 @@
     $db_connection = array();
     $db_connection['host'] = "localhost";
     $db_connection['username'] = "root"; // Change this to your user
-    $db_connection['password'] = "root"; // randomly generated (previously cs546)
+    $db_connection['password'] = ""; // randomly generated (previously cs546)
     $db_connection['database'] = "morpheus_pets"; // randomly generated

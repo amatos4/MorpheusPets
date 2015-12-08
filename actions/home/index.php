39b924e8d9019cb88e48224866d037aa00ff98e8
mpeleshenko <mpeleshenko@gmail.com>
2015-12-07 20:50:36 -0500

    FIX BROKEN CODE

--- actions/home/index.php
+++ actions/home/index.php
@@ -2,14 +2,13 @@
   require_once 'viewmodels/Home_ViewModel.php';
   require_once 'data/data.php';
 
-  $data = MorpheusPetsData::getInstance();
-  $recentUsers = $data->getNRecentUsers(10);
-  // var_dump(recentUsers);
-  //var_dump($data->getNRecentUsers(10));
+  $data        = MorpheusPetsData::getInstance();
+  $recentUsers = $data->getNRecentUsers( 10 );
+
   $viewModel = new Home_ViewModel();
   foreach ( $recentUsers as $rusers )
   {
-      $viewModel->addRecentUsers( $rusers );
+    $viewModel->addRecentUsers( $rusers );
   }
-  // TODO: Complete home action
+
   $viewModel->renderHome();
--- viewmodels/Home_ViewModel.php
+++ viewmodels/Home_ViewModel.php
@@ -3,25 +3,27 @@
 
   class Home_ViewModel extends ViewModel
   {
-    private user_list = [ ];
+    private $user_list = [ ];
+
     /*
      * Render the home page
      */
     public function renderHome()
     {
-      $view_data[ 'page_title' ] = 'Home';
-      $view_data[ 'js' ]         = '<script src="js/index.js"></script>';
-      $view_data[ 'recent_users'] = $user_list;
+      $view_data[ 'page_title' ]   = 'Home';
+      $view_data[ 'js' ]           = '<script src="js/index.js"></script>';
+      $view_data[ 'recent_users' ] = $this->user_list;
 
       $this->renderTemplate( 'templates/header.php', $view_data );
       $this->renderTemplate( 'templates/home_view.php', $view_data );
       $this->renderTemplate( 'templates/footer.php', $view_data );
     }
-  public function addRecentUsers( $users )
+
+    public function addRecentUsers( $users )
     {
       if ( isset( $users ) )
       {
-        array_push( $this->user_list,$users );
+        array_push( $this->user_list, $users );
       }
     }
 
